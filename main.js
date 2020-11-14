9ecb597bb2c4ce842d676ffe7dad31475d82629b
ndjndj <teq.ndj@gmail.com>
2020-11-15 03:49:56 +0900

    getTabs

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const getTabs = () => {
     // 現在のウィンドウのタブをすべて取得する
-    
+    chrome.tabs.query({currentWindow: True}, (tabs) => {
+        console.log(tabs);
+    })
 
 
 }
+
+window.addEventListener('load', () => {
+    getTabs();
+})
