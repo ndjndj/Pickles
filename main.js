a7893cd717fe2be7aae07cf66099609eed6ed2a6
ndjndj <teq.ndj@gmail.com>
2020-11-15 04:09:59 +0900

    getUrl

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const getTabs = () => {
     // 現在のウィンドウのタブをすべて取得する
-    chrome.tabs.query({currentWindow: True}, (tabs) => {
-        console.log(tabs);
-    })
+    
+    let currentTabs = [];
+    chrome.tabs.query(
+        {currentWindow: true}, 
+        (tabs) => {
+            console.log(tabs);
+            for(var i=0; i<tabs.length; i++) {
+                console.log(tabs[i].url);
+            }
+        }
+    )
 
+    console.log(currentTabs);
 
 }
 
