6482dd1ea4ddcbd67a9bf5f13c20ee684157eb08
ndjndj <teq.ndj@gmail.com>
2020-11-16 00:36:30 +0900

    csv

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const operateCurrentTabs = (callback) => {
     chrome.tabs.query(
         {currentWindow: true}, 
         (tabs) => {
-            tabsInfo = tabs.map( (tab, i) => [i+1, tab.title, tab.url] );
+            tabsInfo = tabs.map( (tab, i) => [String(i+1), tab.title, tab.url] );
             // タブの情報をコールバック関数に渡す
             callback(tabsInfo);
         }
@@ -14,15 +14,24 @@ const operateCurrentTabs = (callback) => {
 const downloadTabInfo = (tabsInfo) => {
     // タイトル文字列のエスケープ処理
     let processedTabsInfo = tabsInfo.map(tabs => [tabs[0], escapeForCSV(tabs[1]), tabs[2]]);
-    console.log(processedTabsInfo);
-
-    alert("complete!");
+    // 配列→CSV文字列
+    let csvString = arrToString(processedTabsInfo);
+    console.log(csvString);
+    alert(csvString);
 } 
+
+const arrToString = (arr) => {
+    const colDelimeter = ',';
+    const rowDelimeter = '\n';
+    let s = arr.map((row) => row.join(colDelimeter)).join(rowDelimeter);
+    return s
+}
+
 
 const escapeForCSV = (s) => {
     const targetLst = ['\"', ',', '\\\\n', '\\\\r'];
     const escapedLst = ['\"\"\"', '\",\"', '\"\\n\"', '\"\\r\"'];
-    let escapedValue = s;
+    let escapedValue = String(s);
     targetLst.forEach((elm, i) => {
         let reg = new RegExp(elm, 'g');
         escapedValue = escapedValue.replace(reg, escapedLst[i]);
