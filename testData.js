048fb20e3e9909ea3abee35f968939662e770fca
ndjndj <teq.ndj@gmail.com>
2020-11-19 00:55:55 +0900

    style

--- testData.js
+++ testData.js
@@ -38,7 +38,7 @@ const testArray = [
         , [3, 'Qiita.com', 'https://Qiita.com']
         , [4, 'Qiita.com', 'https://Qiita.com']
         , [5, 'Qiita.com', 'https://Qiita.com']
-        , [6, 'aタグ 新しいタブ - Google 検索','https://www.google.com/search?q=a%E3%82%BF%E3%82%B0+%E6%96%B0%E3%81%97%E3%81%84%E3%82%BF%E3%83%96&oq=a%E3%82%BF%E3%82%B0%E3%80%80%E3%81%82%E3%81%9F%E3%82%89%EF%BD%93aqs=chrome.1.69i57j0i4l2j0i4i5i30l2.15566j0j9&sourceid=chrome&ie=UTF-8']
+        , [6, 'aタグ 新しいタブ - Google 検索','https://www.google.com/search?q=a%E3%82%BF%E3%82%B0+%E6%96%B0%E3%81%97%E3%81%84%E3%82%BF%E3%83%96&']
 ];
 
 arrToHTMLTag(testArray);
