7f0172ddae62ec71d34929ca2f92097fb752e3ab
ndjndj <teq.ndj@gmail.com>
2020-12-07 02:50:35 +0900

    copy

--- output-md.js
+++ output-md.js
@@ -6,14 +6,29 @@ const getMD = (callback) => {
 
 }
 
-const log = (s) => {
-    console.log(s);
+const copyClipboard = (id) => {
+    let target = document.getElementById(id);
+    target.querySelector();
+    document.execCommand('copy');
+}
+
+const pasteMDString = (mdString) => {
+    let textarea = document.getElementById('output-md');
+    textarea.innerText = mdString;
+}
+
+const closeWindow = () => {
+    window.close();
 }
 
 const run = () => {
     window.addEventListener('load', () => {
-        getMD(log);
+        getMD(pasteMDString);
+        const copyButton = document.getElementById('copy');
+        const exitButton = document.getElementById('exit');
 
+        copyButton.addEventListener('click', () => {copyClipboard('output-md');});
+        exitButton.addEventListener('click', () => {closeWindow();});
     });
 }
 
