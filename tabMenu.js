18aa87c75d801c19c8b959e758c59332507a5a1e
ndjndj <teq.ndj@gmail.com>
2020-12-08 01:48:40 +0900

    showTabs

--- tabMenu.js
+++ tabMenu.js
@@ -7,12 +7,39 @@ const getTabsInfo = (callback) => {
 
 const showTabs = (tabsObj) => {
     const keys = Object.keys(tabsObj);
+    const parent = document.getElementById('tabsInfo');
+    
+    let div;
+    let tabName;
+    let del;
+    keys.forEach( (key) => {
+        // コンテナ作成
+        div = document.createElement('div');
+        div.className = 'container';
 
-    console.log(keys);
+        // タブ名作成
+        tabName = document.createElement('a');
+        tabName.className = 'tabName';
+        tabName.innerText = tabsObj[key]['tabName'];
+        // 削除ボタン作成
+        del = document.createElement('a');
+        del.className = 'del';
+        del.innerText = 'DEL';
 
+        div.appendChild(tabName);
+        div.appendChild(del);
+        
+        parent.appendChild(div);
+    });
+    
     for (var i = 0; i < keys.length; i++) {
         console.log(tabsObj[keys[i]]);
+
+
     }
+
+
+
 }
 
 const run = () => {
