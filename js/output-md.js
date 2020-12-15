6ddada0bd5b1c10468f74045d9a44854543c4bf9
ndjndj <teq.ndj@gmail.com>
2020-12-15 13:32:26 +0900

    bug fix

--- js/output-md.js
+++ js/output-md.js
@@ -1,21 +1,23 @@
 const getMD = (callback) => {
     // storage のデータを取得
     chrome.storage.local.get(['mdString'], (result) => {
-        console.log(result.mdString);
         callback(result.mdString);
     });
 
 }
 
 const copyClipboard = (id) => {
+    // クリップボードにコピー
     let target = document.getElementById(id);
     target.select();
     document.execCommand('copy');
 }
 
 const pasteMDString = (mdString) => {
+    // textarea の値を任意の文字列に変更
     let textarea = document.getElementById('output-md');
-    textarea.value = mdString;
+    // 任意の文字列に何もなければ、何も表示しない。
+    textarea.value = mdString == 'undefined' ? '' : mdString;
 }
 
 const closeWindow = () => {
